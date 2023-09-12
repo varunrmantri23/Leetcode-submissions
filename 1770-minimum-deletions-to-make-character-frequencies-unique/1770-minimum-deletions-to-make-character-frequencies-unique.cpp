ae7ce741a28438ef45e93cad4b2bf92b0afea244
Varun Mantri <39723815+varunrmantri23@users.noreply.github.com>
2023-09-12 13:57:41 +0530

    Time: 46 ms (87.37%), Space: 17.4 MB (37.86%) - LeetHub

--- 1770-minimum-deletions-to-make-character-frequencies-unique/1770-minimum-deletions-to-make-character-frequencies-unique.cpp
+++ 1770-minimum-deletions-to-make-character-frequencies-unique/1770-minimum-deletions-to-make-character-frequencies-unique.cpp
@@ -1,35 +1,31 @@
 class Solution {
 public:
     int minDeletions(string s) {
-        unordered_map<char,int> charfrequencies;
-
-        unordered_set<int> existingfrequencies;
-
-        for( char c :s){
-            charfrequencies[c]++;
+        // Create an array to store character frequencies (one for each letter of the alphabet)
+        vector<int> charFrequency(26) ;
+        
+        // Create a set to keep track of existing frequencies in new string
+        unordered_set<int> existingFrequencies;
+        
+        // Count character frequencies
+        for (char c : s) {
+            charFrequency[c - 'a']++;
         }
-
-        priority_queue<int> maxheap;
-
-        for( auto freq : charfrequencies){
-            maxheap.push(freq.second);
-            // cout<<freq.first<<endl;
-            // cout<<freq.second<<endl;
-        }
-
-        int deletions = 0;
-        while(!maxheap.empty()){
-            int currfreq = maxheap.top();
-            maxheap.pop();
-
-            while(currfreq > 0 && existingfrequencies.count(currfreq)){
-                currfreq--;
-                deletions++;
+        
+        int deletions = 0 ;
+        // Iterate through the sorted frequencies
+        for(int i = 0 ; i < charFrequency.size() ; i ++){
+            int curFreq = charFrequency[i] ;
+            
+            // Adjust frequency if it already exists in the new string
+            while(curFreq > 0 && existingFrequencies.find(curFreq) != existingFrequencies.end()){
+                deletions ++ ;
+                curFreq -- ;
             }
-            existingfrequencies.insert(currfreq);
-
+            // Add the adjusted frequency to the set
+            existingFrequencies.insert(curFreq) ;
         }
-         return deletions;
 
+        return deletions;
     }
 };

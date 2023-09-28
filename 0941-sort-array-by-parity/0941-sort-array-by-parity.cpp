fc1b6e45c19c1c4a844ceaba1d792ce4ac39dec6
Varun Mantri <39723815+varunrmantri23@users.noreply.github.com>
2023-09-28 22:27:46 +0530

    Time: 9 ms (44.99%), Space: 16.5 MB (25.63%) - LeetHub

--- 0941-sort-array-by-parity/0941-sort-array-by-parity.cpp
+++ 0941-sort-array-by-parity/0941-sort-array-by-parity.cpp
@@ -3,19 +3,21 @@ public:
     vector<int> sortArrayByParity(vector<int>& nums) {
         int p1 = 0;
         int n = nums.size();
-        for(int p2 = 0; p2< n; p2++){
-            if(p1 <= p2){
-                if(nums[p2] % 2 == 0 && nums[p1] % 2 != 0){
-                    nums[p1] = nums[p1] + nums[p2];
-                    nums[p2] = nums[p1] - nums[p2];
-                    nums[p1] = nums[p1] - nums[p2];
+        
+        for (int p2 = 0; p2 < n; p2++) {
+            if (p1 <= p2) {
+                if (nums[p2] % 2 == 0 && nums[p1] % 2 != 0) {
+                    // Swap elements without using extra memory
+                    nums[p1] ^= nums[p2];
+                    nums[p2] ^= nums[p1];
+                    nums[p1] ^= nums[p2];
                     p1++;
-                }
-                else if(nums[p1]%2 == 0){
+                } else if (nums[p1] % 2 == 0) {
                     p1++;
                 }
             }
         }
+        
         return nums;
     }
 };

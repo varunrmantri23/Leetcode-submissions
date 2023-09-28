9aee275f1aabb996566f4fffb6d851dedde909e4
Varun Mantri <39723815+varunrmantri23@users.noreply.github.com>
2023-09-28 22:05:35 +0530

    Time: 3 ms (96.18%), Space: 16.5 MB (36.74%) - LeetHub

--- 0941-sort-array-by-parity/0941-sort-array-by-parity.cpp
+++ 0941-sort-array-by-parity/0941-sort-array-by-parity.cpp
@@ -5,14 +5,15 @@ public:
         for(int p2 = 0; p2< nums.size(); p2++){
             if(p1 <= p2){
                 if(nums[p2] % 2 == 0 && nums[p1] % 2 != 0){
-                    swap(nums[p1],nums[p2]);
+                    nums[p1] = nums[p1] + nums[p2];
+                    nums[p2] = nums[p1] - nums[p2];
+                    nums[p1] = nums[p1] - nums[p2];
                     p1++;
                 }
                 else if(nums[p1]%2 == 0){
                     p1++;
                 }
             }
-            
         }
         return nums;
     }

80577d7f3c9e660baf869eff442eda39450544c0
Varun Mantri <39723815+varunrmantri23@users.noreply.github.com>
2023-10-13 00:16:29 +0530

    Time: 80 ms (80.44%), Space: 60 MB (20.34%) - LeetHub

---
+++ 0882-peak-index-in-a-mountain-array/0882-peak-index-in-a-mountain-array.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int peakIndexInMountainArray(vector<int>& arr) {
+        int n = arr.size();
+        int ans = binaryhelper(arr,0,n-1);
+        return ans;
+    }
+    int binaryhelper(vector<int>&arr, int start, int end){
+        int m = arr.size();
+        int mid = 0;
+        if (start <= end) {
+            int mid = (start + end) / 2;
+            
+            if (arr[mid] > arr[mid - 1] && arr[mid] > arr[mid + 1]) {
+                return mid;
+            } else if (arr[mid] > arr[mid - 1] && arr[mid] < arr[mid + 1]) {
+                return binaryhelper(arr, mid, end);
+            } else {
+                return binaryhelper(arr, start, mid);
+            }
+        }
+        return -1;
+    }
+};

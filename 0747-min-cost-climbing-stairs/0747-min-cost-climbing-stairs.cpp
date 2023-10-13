edfb6da318f20b5b84d3e8eb09f3d9c45fafe0c3
Varun Mantri <39723815+varunrmantri23@users.noreply.github.com>
2023-10-13 14:37:21 +0530

    Time: 0 ms (100%), Space: 13.9 MB (37.3%) - LeetHub

--- 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.cpp
+++ 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.cpp
@@ -1,20 +1,19 @@
+#include <vector>
 #include <algorithm>
+
 class Solution {
 public:
-    int minCostClimbingStairs(vector<int>& cost) {
+    int minCostClimbingStairs(std::vector<int>& cost) {
         int n = cost.size();
-        vector<int> dp(n, 0);
+        int oneStep = cost[1];
+        int twoStep = cost[0];
 
-        // Handle the first two elements separately
-        dp[0] = cost[0];
-        dp[1] = cost[1];
-
-        // Calculate the minimum cost for the rest of the elements
         for (int i = 2; i < n; i++) {
-            dp[i] = cost[i] + std::min(dp[i - 1], dp[i - 2]);
+            int currentStep = cost[i] + std::min(oneStep, twoStep);
+            twoStep = oneStep;
+            oneStep = currentStep;
         }
 
-        // Return the minimum cost to reach the top
-        return std::min(dp[n - 1], dp[n - 2]);
+        return std::min(oneStep, twoStep);
     }
 };

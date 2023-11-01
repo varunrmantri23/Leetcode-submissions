aec5188c67149e6154a306eb5e277c1ce313ba89
Varun Mantri <39723815+varunrmantri23@users.noreply.github.com>
2023-11-01 18:23:04 +0530

    Time: 17 ms (52.27%), Space: 26.1 MB (12.37%) - LeetHub

--- 0501-find-mode-in-binary-search-tree/0501-find-mode-in-binary-search-tree.cpp
+++ 0501-find-mode-in-binary-search-tree/0501-find-mode-in-binary-search-tree.cpp
@@ -1,36 +1,42 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
+    map<int,int> m;
 public:
     vector<int> findMode(TreeNode* root) {
-        unordered_map<int, int> counter;
-        vector<TreeNode*> stack;
-        stack.push_back(root);
-        
-        while (!stack.empty()) {
-            TreeNode* node = stack.back();
-            stack.pop_back();
-
-            counter[node->val]++;
-            if (node->left != nullptr) {
-                stack.push_back(node->left);
-            }
-            if (node->right != nullptr) {
-                stack.push_back(node->right);
-            }
-        }
-        
-        int maxFreq = 0;
-
-        for (auto& [key, val] : counter) {
-            maxFreq = max(maxFreq, val);
-        }
-        
+        if(root->right == NULL && root-> left == NULL) return {root->val};
+        findModeHelper(root);
         vector<int> ans;
-        for (auto& [key, val] : counter) {
-            if (val == maxFreq) {
-                ans.push_back(key);
-            } 
+        int max_ = 0;
+        for(auto a:m){
+            if(a.second>0)
+            {
+                max_ = max(max_,a.second);
+            }
         }
-        
+        for(auto a:m){
+            if(a.second==max_)
+                ans.push_back(a.first);
+        }
         return ans;
     }
+    void findModeHelper(TreeNode* root) {
+        if(root == NULL)return ;
+
+        m[root->val]++;
+        if(root->right != NULL);
+        findModeHelper(root->right);
+        if(root->left != NULL)
+        findModeHelper(root->left);
+    }
+
 };

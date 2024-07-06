24fe2a1a751c57cb20245ef4697fa7b6b442dfac
Varun Mantri <39723815+varunrmantri23@users.noreply.github.com>
2024-07-06 23:55:11 +0530

    Time: 2 ms (54.14%), Space: 7 MB (71.18%) - LeetHub

---
+++ 2582-pass-the-pillow/2582-pass-the-pillow.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    int passThePillow(int n, int time) {
+        if((time/(n -1)) % 2 == 0){
+            return (time%(n-1)) + 1;
+        }
+        else return n - (time%(n-1)) ;
+    }
+};

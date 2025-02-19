38f9bebf48731a3ad8f898c432b2ea37a6511a00
Varun Mantri <39723815+varunrmantri23@users.noreply.github.com>
2025-02-19 23:35:25 +0530

    Time: 55 ms (9.32%), Space: 27.9 MB (7.42%) - LeetHub

--- 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.cpp
+++ 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.cpp
@@ -1,32 +1,34 @@
 class Solution {
 public:
+    vector<string> s; // Keeping s as a class member
+
     string getHappyString(int n, int k) {
-        vector<string> s; 
-        helper(1, "a", 'a', n, s);
-        helper(1, "b", 'b', n, s);
-        helper(1, "c", 'c', n, s);
+       
+        helper(1, "a", 'a', n);
+        helper(1, "b", 'b', n);
+        helper(1, "c", 'c', n);
 
         sort(s.begin(), s.end());
         if (s.size() >= k) return s[k - 1]; 
         return "";
     }
 
-    void helper(int len, string k, char a, int n, vector<string>& s) {
+    void helper(int len, string k, char a, int n) {
         if (len == n) {
             s.push_back(k);
             return;
         }
         if (a == 'a') {
-            helper(len + 1, k + 'b', 'b', n, s);
-            helper(len + 1, k + 'c', 'c', n, s);
+            helper(len + 1, k + 'b', 'b', n);
+            helper(len + 1, k + 'c', 'c', n);
         }
         if (a == 'b') {
-            helper(len + 1, k + 'a', 'a', n, s);
-            helper(len + 1, k + 'c', 'c', n, s);
+            helper(len + 1, k + 'a', 'a', n);
+            helper(len + 1, k + 'c', 'c', n);
         }
         if (a == 'c') {
-            helper(len + 1, k + 'a', 'a', n, s);
-            helper(len + 1, k + 'b', 'b', n, s);
+            helper(len + 1, k + 'a', 'a', n);
+            helper(len + 1, k + 'b', 'b', n);
         }
     }
 };

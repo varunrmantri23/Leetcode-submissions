6e2875a2f0ea8a3092fa4caa7ff763035574bc32
Varun Mantri <39723815+varunrmantri23@users.noreply.github.com>
2025-03-27 02:59:25 +0530

    Time: 48 ms (38.86%), Space: 89.9 MB (62.35%) - LeetHub

---
+++ 2160-minimum-operations-to-make-a-uni-value-grid/2160-minimum-operations-to-make-a-uni-value-grid.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int minOperations(vector<vector<int>>& grid, int x) {
+        //we need somewhere in mid to make less operations, sort -> mid , 
+        //median <>
+        //So find Floor(L/2) -> make every element as that 
+        //a+3x = target that means target - a / x is divisiible, so % = 0 
+        //target - a = kx
+        //target%x = a%x
+        int n = grid.size();
+        int m = grid[0].size();
+        int operations = 0;
+        vector <int> vec;
+        for(int i = 0; i < n;i++){
+            for(int j = 0; j < m;j++)
+                vec.push_back(grid[i][j]);
+        }
+        //sort
+        sort(vec.begin(), vec.end());
+        int L = vec.size(); //n*m
+        int target = vec[L/2];
+        //cout << target << endl;
+        
+        for(int &num : vec){
+            if(num%x != target%x) 
+                return -1;
+            else{
+                operations += abs(num-target)/x;
+            }
+        }
+
+        return operations;
+    }
+};

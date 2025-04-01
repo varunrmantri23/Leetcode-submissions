0507305bee4ba936cfc64a9265290380fde79581
Varun Mantri <39723815+varunrmantri23@users.noreply.github.com>
2025-04-01 10:00:01 +0530

    Time: 12 ms (28.33%), Space: 127.7 MB (16.19%) - LeetHub

--- 2262-solving-questions-with-brainpower/2262-solving-questions-with-brainpower.cpp
+++ 2262-solving-questions-with-brainpower/2262-solving-questions-with-brainpower.cpp
@@ -15,14 +15,3 @@ public:
         return dp[i] = max(take, dont);
     }
 };
-/* Hint 1
-For each question, we can either solve it or skip it. How can we use Dynamic Programming to decide the most optimal option for each problem?
-Hint 2
-We store for each question the maximum points we can earn if we started the exam on that question.
-Hint 3
-If we skip a question, then the answer for it will be the same as the answer for the next question.
-Hint 4
-If we solve a question, then the answer for it will be the points of the current question plus the answer for the next solvable question.
-Hint 5
-The maximum of these two values will be the answer to the current question.
-*/

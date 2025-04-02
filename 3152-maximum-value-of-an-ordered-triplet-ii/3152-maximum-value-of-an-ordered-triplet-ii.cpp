1a28ebeb265e8da28f1ca655b6b0869448e7617e
Varun Mantri <39723815+varunrmantri23@users.noreply.github.com>
2025-04-02 20:37:14 +0530

    Time: 3 ms (73.31%), Space: 90.9 MB (97.07%) - LeetHub

--- 3152-maximum-value-of-an-ordered-triplet-ii/3152-maximum-value-of-an-ordered-triplet-ii.cpp
+++ 3152-maximum-value-of-an-ordered-triplet-ii/3152-maximum-value-of-an-ordered-triplet-ii.cpp
@@ -1,34 +1,15 @@
 class Solution {
 public:
     long long maximumTripletValue(vector<int>& nums) {
-        //we want i to be maximum, then k and minimum j
-        //what we can do is pre - compute left max and right max of any j, and then iterate over and calculate maximum value
+        //optimal approach - greedy
+        //just focus on maximising the diffeence of i and j, and we can fix all k and check 
         int n = nums.size();
-        if(n < 3) return 0;
-
-        long long tmax = nums[0];
-        long long tmax_ = nums[n-1];
-        
-        vector <int> Lmax(n, 0);
-        vector <int> Rmax(n, 0);
-        
-        Lmax[0] = -1;
-        for(int i = 1; i < n;i++){
-            Lmax[i] = tmax;
-            tmax = max(tmax, (long long)nums[i]);
+        long long answer = 0, imax = 0, dmax = 0;
+        for(int k = 0; k < n; k++){
+            answer = max(answer, (long long)dmax * nums[k]);
+            dmax = max(dmax, (long long)imax - nums[k]);
+            imax = max(imax, (long long)nums[k]);
         }
-        
-        Rmax[n-1] = -1;
-        for(int j = n -2; j >= 0; j--){
-            Rmax[j] = tmax_;
-            tmax_ = max(tmax_, (long long)nums[j]);
-        }
-        
-        long long answer = 0;
-        for(int k = 1; k < n - 1; k++){
-            answer = max(answer, (long long)(Lmax[k] - nums[k]) * Rmax[k]);
-        }
-        
         return answer;
     }
 };

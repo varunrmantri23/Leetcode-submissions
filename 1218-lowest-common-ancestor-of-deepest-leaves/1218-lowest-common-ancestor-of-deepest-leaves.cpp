821bcd0bde32ac49c1c61d50e22441729a6bfb64
Varun Mantri <39723815+varunrmantri23@users.noreply.github.com>
2025-04-05 01:22:32 +0530

    Time: 0 ms (100%), Space: 22 MB (67.5%) - LeetHub

--- 1218-lowest-common-ancestor-of-deepest-leaves/1218-lowest-common-ancestor-of-deepest-leaves.cpp
+++ 1218-lowest-common-ancestor-of-deepest-leaves/1218-lowest-common-ancestor-of-deepest-leaves.cpp
@@ -9,26 +9,44 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+ //The values of the nodes in the tree are unique.
 class Solution {
 public:
     TreeNode* lcaDeepestLeaves(TreeNode* root) {
-        return dfs(root).first;
+        //every node i need to check l subtree and r subtree and decide whether current node is lca or the subtree itself had inside , either l or r subtree has depth node then subtree can have lca
+
+        // //calculate depth first and then on max depth we will do lca logic
+        // //map node-> depth
+        // unordered_map<int, int> depth;
+        // dfsDepth(root, 0, depth);
+        // // for(auto &i : depth){
+        // //     cout << i.first << " " << i.second << endl;
+        // // }
+        
+        int maxDepth = getDepth(root);
+        return getLCA(root, 0, maxDepth);
     }
-    
-    // Returns a pair: {LCA of deepest leaves in this subtree, depth of this subtree}
-    pair<TreeNode*, int> dfs(TreeNode* node) {
-        if (!node)
-            return {nullptr, 0};
-        
-        auto left = dfs(node->left);
-        auto right = dfs(node->right);
-        
-        // If both subtrees have the same depth, then current node is the LCA.
-        if (left.second == right.second)
-            return {node, left.second + 1};
-        else if (left.second > right.second)
-            return {left.first, left.second + 1};
-        else
-            return {right.first, right.second + 1};
+    //macx depth
+    int getDepth(TreeNode* node) {
+        if (!node) return 0;
+        return 1 + max(getDepth(node->left), getDepth(node->right));
+    }
+
+    // void dfsDepth(TreeNode* x, int d,unordered_map<int, int> &depth){
+    //     if(x == nullptr) return;
+    //     depth[x->val] = d;
+    //     dfsDepth(x->left, d+1,depth);
+    //     dfsDepth(x->right, d+1,depth);
+    // }
+    //get lca
+     TreeNode* getLCA(TreeNode* n, int depth, int maxDepth) {
+        if (!n) return nullptr;
+        if (depth == maxDepth - 1 && !n->left && !n->right) return n;
+
+        TreeNode* left = getLCA(n->left, depth + 1, maxDepth);
+        TreeNode* right = getLCA(n->right, depth + 1, maxDepth);
+
+        if (left && right) return n;
+        return left ? left : right;
     }
 };

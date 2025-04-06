834ee41de5792664db99c2e01824558fa390bc86
Varun Mantri <39723815+varunrmantri23@users.noreply.github.com>
2025-04-07 02:52:29 +0530

    Time: 15 ms (30.51%), Space: 12.6 MB (46.78%) - LeetHub

--- 0368-largest-divisible-subset/0368-largest-divisible-subset.cpp
+++ 0368-largest-divisible-subset/0368-largest-divisible-subset.cpp
@@ -1,33 +1,22 @@
-
 class Solution {
 public:
-    vector <vector<int>> cache;
     vector<int> largestDivisibleSubset(vector<int>& nums) {
         sort(nums.begin(), nums.end());
-        int n = nums.size();
-        cache = vector<vector<int>>(n);
-        vector<int> res = {};
-        for(int k = 0; k < nums.size();k++){
-            vector<int> temp = recursiveHelper(k, nums);
-            if(temp.size() > res.size()) res = temp;
-        }
-        return res;
-    }
-    vector<int> recursiveHelper(int i, vector<int> &nums){
-        // if(i == nums.size() - 1) return {};
-        if(!cache[i].empty()) return cache[i];
-        vector<int> res;
-        for(int j = i+1; j < nums.size();j++){
-            if(nums[j] % nums[i] == 0){
-                vector<int> temp =recursiveHelper(j, nums);
-                if(temp.size() > res.size()) res = temp;
+        vector<int> dp(nums.size(), 1), prev(nums.size(), -1);
+        int maxi = 0;
+        for (int i = 1; i < nums.size(); i++) {
+            for (int j = 0; j < i; j++) {
+                if (nums[i] % nums[j] == 0 && dp[i] < dp[j] + 1) {
+                    dp[i] = dp[j] + 1;
+                    prev[i] = j;
+                }
             }
+            if (dp[i] > dp[maxi]) maxi = i;
         }
-        res.insert(res.begin(),nums[i]);
-        cache[i] = res;
+        vector<int> res;
+        for (int i = maxi; i >= 0; i = prev[i]) {
+            res.push_back(nums[i]);
+        }
         return res;
     }
 };
-
-
-

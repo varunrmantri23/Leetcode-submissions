6114dda7b4e1a25ad3469946f8a413a57a05b4ed
Varun Mantri <39723815+varunrmantri23@users.noreply.github.com>
2025-04-11 16:54:57 +0530

    Time: 0 ms (100%), Space: 10.3 MB (46.77%) - LeetHub

--- 3243-count-the-number-of-powerful-integers/3243-count-the-number-of-powerful-integers.cpp
+++ 3243-count-the-number-of-powerful-integers/3243-count-the-number-of-powerful-integers.cpp
@@ -1,45 +1,44 @@
 class Solution {
-
 public:
-    long long count_valid(long long n, int limit) {
-        string str = to_string(n);
-        int len = str.size();
-        vector<vector<long long>> dp(len+1, vector<long long>(2, -1));
-        
-        function<long long(int,int)> rec = [&](int pos, int tight) -> long long {
-            if(pos == len)
-                return 1;
-            if(dp[pos][tight] != -1) return dp[pos][tight];
-            long long ways = 0;
-            int up = (tight ? (str[pos]-'0') : limit);
-            // adjust if the current digit of n is higher than limit in a tight state:
-            if(tight && (str[pos]-'0') > limit)
-                up = limit;
-            for(int d = 0; d <= up; d++){
-                int ntight = (tight && (d == (str[pos]-'0')));
-                ways += rec(pos+1, ntight);
-            }
-            dp[pos][tight] = ways;
-            return ways;
-        };
-        
-        return rec(0, 1);
-    }
-    // returns count of powerful integers <= x
-    long long solve(long long x, int limit, string s) {
-        long long suf = stoll(s);
-        int m = s.size();
-        long long base = 1;
-        for (int i = 0; i < m; i++) {
-            base *= 10;
-        }
-        if (x < suf) return 0;
-        long long tmax = (x - suf) / base;
-        return count_valid(tmax, limit);
-    }
-    
     long long numberOfPowerfulInt(long long start, long long finish, int limit, string s) {
-        return solve(finish, limit, s) - solve(start - 1, limit, s);
+        //we know it ends with specific suffix so the prefix will have fixed possibilty
+        //__ 124
+        //so we can see first blank should range between given start and finish and also that digit has a limit
+        //solve(finish) - solve(start-1) solve starts from 0 so we get our ans 
+        //to string , easier to iterate
+        
+        //remaining length = finish.length - s.length
+        
+        string strt = to_string(start-1);
+        string end = to_string(finish);
+        
+        return solve(end,s,limit) - solve(strt,s,limit);
     }
-};
 
+    long long solve(string &str, string &s, int limit){
+        if(str.length() < s.length()) return 0;
+        long long count = 0;
+        string subStr = str.substr(str.length() - s.length());
+        int remainingLen = str.length() - s.length();
+        for(int i = 0; i < remainingLen; i++){
+            int digit = str[i] - '0';
+
+            if(digit <= limit ){
+                count += digit * pow(limit+1, remainingLen - i -1);
+            }
+            else{
+                count += pow(limit+1, remainingLen -i);
+                return count;
+            }
+            
+        }
+
+
+
+        if(subStr >= s){
+            count+=1;
+        }
+
+        return count;
+    };
+};

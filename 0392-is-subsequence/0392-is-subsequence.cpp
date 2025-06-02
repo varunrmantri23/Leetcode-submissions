71b9d0429e72975f2294efb5fb7269b4f9424c15
Varun Mantri <39723815+varunrmantri23@users.noreply.github.com>
2025-06-02 14:18:56 +0530

    Time: 0 ms (100%), Space: 9.1 MB (5.93%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.cpp
+++ 0392-is-subsequence/0392-is-subsequence.cpp
@@ -1,20 +1,24 @@
 class Solution {
 public:
     bool isSubsequence(string s, string t) {
-        int n = s.length(),m=t.length();
-        int j = 0; 
-    // For index of s (or subsequence
- 
-    // Traverse s and t, and
-    // compare current character
-    // of s with first unmatched char
-    // of t, if matched
-    // then move ahead in s
-    for (int i = 0; i < m and j < n; i++)
-        if (s[j] == t[i])
-            j++;
- 
-    // If all characters of s were found in t
-    return (j == n);
+            int n = s.length();
+            int m = t.length();
+            int dp[n + 1][m + 1];
+            for (int i = 0; i < n + 1; i++)
+                dp[i][0] = 0;
+            for (int j = 0; j < m + 1; j++)
+                dp[0][j] = 0;
+
+            for (int i = 1; i < n + 1; i++) {
+                for (int j = 1; j < m + 1; j++) {
+                    if (s[i - 1] == t[j - 1]) {
+                        dp[i][j] = 1 + dp[i - 1][j - 1];
+                    } else {
+                        dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+                    }
+                }
+            }
+            return dp[n][m] == s.length();
+        
     }
 };

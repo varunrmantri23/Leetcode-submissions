20926b57aa8666aeb0ab96a4dce31df92e8bb277
Varun Mantri <39723815+varunrmantri23@users.noreply.github.com>
2025-07-11 11:02:29 +0530

    Time: 73 ms (66.54%), Space: 98.1 MB (91.68%) - LeetHub

--- 2479-meeting-rooms-iii/2479-meeting-rooms-iii.cpp
+++ 2479-meeting-rooms-iii/2479-meeting-rooms-iii.cpp
@@ -2,37 +2,44 @@ class Solution {
 public:
     int mostBooked(int n, vector<vector<int>>& meetings) {
         //sort meetings so that we can assign based on start times
+        int m = meetings.size();
         sort(meetings.begin(), meetings.end());
         // need to store when room finishes in some ds
         // min-heap for when room ends
         //if found empty round, assign, if not, then select next free room which gets free first among others
-        vector<long long> lastAvailableAt(n,0);
         vector<int> roomUsedCount(n,0); //room used how many times
-        for(vector<int> &meeting: meetings){
-            int start = meeting[0];
-            int end = meeting[1];
-            bool found = false;
-            long long earlyEndRoomTime = LLONG_MAX;
-            int earlyEndRoom = 0;
-            //find first available meeting room
-            for(int room = 0; room < n; room++){
-                if(lastAvailableAt[room] <= start){
-                    lastAvailableAt[room] = end;
-                    roomUsedCount[room]++;
-                    found = true;
-                    break;
-                }
-                if(lastAvailableAt[room] < earlyEndRoomTime){
-                    earlyEndRoom = room;
-                    earlyEndRoomTime = lastAvailableAt[room];
-                }
+        priority_queue<pair<long long,int>, vector<pair<long long,int>>, greater<pair<long long,int>>> usedRooms;
+        priority_queue<int, vector<int>, greater<int>> availableRooms;
+
+        for(int room = 0; room < n; room++){
+            availableRooms.push(room);
+        }
+        //all rooms are available initially so push
+        for(vector<int> &meet : meetings){
+            int start = meet[0];
+            int end = meet[1];
+
+            //first check all used rooms which are free and shift to free
+            while(!usedRooms.empty() && usedRooms.top().first <= start){
+                int room = usedRooms.top().second;
+                usedRooms.pop();
+                availableRooms.push(room);
             }
-            //if not found room
-            if(!found){
-                //pick first free time
-                lastAvailableAt[earlyEndRoom] += end - start;
-                roomUsedCount[earlyEndRoom]++;
+            if(!availableRooms.empty()){
+                int room = availableRooms.top();
+                availableRooms.pop();
+                usedRooms.push({end, room});
+                roomUsedCount[room]++;
             }
+            else {
+                //no room empty rn
+                int room = usedRooms.top().second;
+                long long endTime = usedRooms.top().first;
+                usedRooms.pop();
+                usedRooms.push({endTime + (end-start) ,room});
+                roomUsedCount[room]++;
+            }
+
         }
         int resultRoom = -1;
         int maxUse = 0;
